c33716bde6ea794f04c469bca2921aad6b7e2428
seneka-DSNA <alejandroaguilartorices@gmail.com>
2026-01-29 19:13:24 +0100

    Update product_repository.py

--- product_repository.py
+++ product_repository.py
@@ -2,13 +2,48 @@ def list_active_products():
     return [
         {
             "id": 1,
-            "name": "lel",
-            "price": 50.00
+            "name": "S.Pintar",
+            "price": 10.00
         },
         {
             "id": 2,
-            "name": "lol",
+            "name": "Pintada",
+            "price": 40.00
+        },
+        {
+            "id": 3,
+            "name": "Solo.Texturizada",
             "price": 20.00
+        },
+        {
+            "id": 4,
+            "name": "Pintada.Texturizada",
+            "price": 60.00
+        },
+        {
+            "id": 5,
+            "name": "Pin",
+            "price": 5.00
+        },
+        {
+            "id": 6,
+            "name": "Llavero",
+            "price": 4.00
+        },
+        {
+            "id": 7,
+            "name": "Llavero D.R.Y",
+            "price": 5.00
+        },
+        {
+            "id": 8,
+            "name": "Soporte",
+            "price": 30.00
+        },
+        {
+            "id": 9,
+            "name": "Pines pack",
+            "price": 10.00
         },
     ]
 
